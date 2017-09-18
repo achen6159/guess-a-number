00b7b2b22b490facf3ca3c61e04a1a91cb2f9f19
Chen <achen6159@greenville.k12.sc.us>
2017-09-18 09:28:28 -0400

    Let's you play the game again

--- guess_a_number.py
+++ guess_a_number.py
@@ -5,42 +5,57 @@ low = 1
 high = 100
 limit = 10
 
-
-#start game
-rand = random.randint(low, high)
-print("I'm thinking of a number from " + str(low) + " to " + str(high));
-
-guess = -1
-tries = 0
-
 #helper functions
 def get_guess():
     while True:
-        g = input("Take a guess: ")
+        guess = input("Take a guess: ")
 
-        if g.isnumeric():
-            g = int(g)
-            return g
+        if guess.isnumeric():
+            guess = int(guess)
+            return guess
         else:
             print("You must enter a number")
 
-#play the game
-while guess != rand and tries < limit:
-    guess = get_guess()
+def play_again():
+    while True:
+        decision = input("Would you like to play again? (y/n) ")
+
+        if decision == "y" or decision == "yes":
+            return True
+        elif decision == "n" or decision == "no":
+            return False
+
+        print("I don't understand. Please answer 'y' or 'n')
+
+again = True
+              
+while again:
+    #start game
+    rand = random.randint(low, high)
+    print("I'm thinking of a number from " + str(low) + " to " + str(high));
+
+    guess = -1
+    tries = 0
+
+    #play the game
+    while guess != rand and tries < limit:
+        guess = get_guess()
     
-    if guess < rand:
-        print("You guessed too low.")
-    elif guess > rand:
-        print("You guessed too high.")
+        if guess < rand:
+            print("You guessed too low.")
+        elif guess > rand:
+            print("You guessed too high.")
 
-    tries += 1  
+        tries += 1
+    #end the game
+    if guess == rand:
+        print("You win.")
+    else:
+        print("Aww...too bad. The number I was thinking of was " + str(rand) + ".")
 
-#tell player outcome of the game
-if guess == rand:
-    print("You win.")
-else:
-    print("Aww...too bad. The number I was thinking of was " + str(rand) + ".")
+    again = play_again()
 
+print("Goodbye")
 
 
 
